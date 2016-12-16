db257d25582afde82cfb8747f9c347fb6c08f100
Dennis Zelada <dennis.zelada@modomoto.de>
2016-12-16 15:27:02 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My SoundCloud React Redux App';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+
